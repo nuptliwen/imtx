be8180e78e0c90126d90995a7c0b477920fc8c25
Tualatrix Chou <tualatrix@gmail.com>
2012-05-29 10:01:31 +0800

    Oops, fix the pm2.5 path

--- imtx/scripts/update_pm25.sh
+++ imtx/scripts/update_pm25.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/tualatrix/public_html/imtx.me/imtx/imtx/
+cd /home/tualatrix/public_html/imtx.me/imtx
 workon imtx.me
 /home/tualatrix/public_html/imtx.me/bin/python manage.py update_pm25
