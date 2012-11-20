1cacbde88c2d72a81e7eec1e1daade140d856190
Tualatrix Chou <tualatrix@gmail.com>
2012-11-20 22:30:35 +0800

    Make fabfile work with sync

--- fabfile.py
+++ fabfile.py
@@ -8,44 +8,49 @@ from fabric.contrib.console import confirm
 env.user = os.getenv('IMTX_USER')
 env.hosts = [os.getenv('IMTX_HOST')]
 
-def get_mysql_password():
-    sys.path.insert(0, os.getcwd())
-    import imtx.settings
-    return imtx.settings.DATABASES['default']['PASSWORD']
+def createdb(password, database):
+    local('mysql -uroot -p%s -e "create database %s"' % (password, database))
 
-def createdb():
-    local('mysql -uroot -p%s -e "create database imtx_me"' % get_mysql_password())
-
-def dropdb():
-    local('mysql -uroot -p%s -e "drop database imtx_me"' % get_mysql_password())
+def dropdb(password, database):
+    local('mysql -uroot -p%s -e "drop database %s"' % (password, database))
 
 def sync():
-    if get_mysql_password():
-        try:
-            dropdb()
-        except:
-            pass
-        createdb()
     tar_list = glob.glob(os.path.expanduser('~/Dropbox/imtx-backup/*.tar.gz'))
+
     tar_list.sort()
     tar = tar_list[-1]
 
     local('cd .. && tar zxf %s imtx/imtx/static/' % tar, capture=False)
+    local('cd .. && tar zxf %s imtx/imtx/media/' % tar, capture=False)
     local('cd .. && tar zxf %s imtx/imtx/local_settings.py' % tar, capture=False)
+
+    sys.path.insert(0, os.getcwd())
+    import imtx.settings
+
+    password = imtx.settings.DATABASES['default']['PASSWORD']
+    database = imtx.settings.DATABASES['default']['NAME']
+
+    if password and database:
+        try:
+            dropdb(password, database)
+        except:
+            pass
+        createdb(password, database)
+
     if os.uname()[0] == 'Darwin':
         local("gsed -i 's/DEBUG = False/DEBUG = True/g' imtx/local_settings.py", capture=False)
     else:
         local("sed -i 's/DEBUG = False/DEBUG = True/g' imtx/local_settings.py", capture=False)
 
-    mysql_password = get_mysql_password()
     sql_list = glob.glob(os.path.expanduser('~/Dropbox/imtx-backup/*.sql.gz'))
+
     sql_list.sort()
     sql = sql_list[-1]
     base_name = os.path.basename(sql)
     local('rm -rf /tmp/imtx*')
     local('cp %s /tmp/%s' % (sql, base_name))
     local('gunzip /tmp/%s' % base_name)
-    local('mysql -uroot -p%s imtx_me < /tmp/%s' % (mysql_password, base_name[:-3]))
+    local('mysql -uroot -p%s %s < /tmp/%s' % (password, database, base_name[:-3]))
     local('rm /tmp/%s' % base_name[:-3])
 
 def install():
